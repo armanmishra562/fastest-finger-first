5b76e81af48d7992331ac1f8d9cd32d52a60e895
Arman Mishra <arman2004562@gmail.com>
2025-04-09 14:11:39 +0530

    fix3

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminQuiz from './AdminQuiz';
 import UserQuiz from './UserQuiz';
 import './App.css';
